ab7ed7d2d2e63660ee0282682db4e82df2ce7213
ChloeNogueira <51606452+ChloeNogueira@users.noreply.github.com>
2021-05-31 18:56:00 +0200

    Composant fonctionnel

--- src/Client.jsx
+++ src/Client.jsx
@@ -1,18 +1,28 @@
 import React, { Component } from "react";
 
-class Client extends Component {
-  render() {
-    //const details = this.props.details;
-    //const onDelete = this.props.onDelete;
+/*Composant fonctionnel, cad qu'il n'a pas besoin d'état, de données, 
+il se suffit à lui-même avec les infos envoyées par le composant parent, 
+Donc pas besoin de Class */
 
-    const { details, onDelete } = this.props; //déstructuration
-    return (
-      <li>
-        {details.nom}
-        <button onClick={() => onDelete(details.id)}>X</button>
-      </li>
-    );
-  }
+function Client(props) {
+  //const details = this.props.details;
+  //const onDelete = this.props.onDelete;
+
+  const { details, onDelete } = props; //déstructuration, peut se faire directement dans les params de la fonction à la place de props
+  return (
+    <li>
+      {details.nom}
+      <button onClick={() => onDelete(details.id)}>X</button>
+    </li>
+  );
 }
 
+/*
+const Client = ({details, onDelete}) => (
+    <li>
+      {details.nom}
+      <button onClick={() => onDelete(details.id)}>X</button>
+    </li>
+ );
+*/
 export default Client;
